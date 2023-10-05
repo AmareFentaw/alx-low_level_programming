f068e4ee01f1d0874593777e7df9e4dba28232a7
AmareFentaw <amarefentawdubale38@gmail.com>
2023-10-05 12:13:25 -0700

    4th

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 
 /**
- *Aprogram of put function
+ *A program to print a dexcription  followed by a new line.
  *Return: (0) (done)
  */
 #include <stdio.h>
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle,");
+puts("\"Programming is like building a multilingual puzzle,");
 return (0);
 }
