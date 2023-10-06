0c4d25cb6d4797e247a2f1f07b7500dbfeae21ef
AmareFentaw <anarefentawdubale38@gmail.com>
2023-10-06 09:36:50 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,7 @@
+#!/bin/bash
+int main()
+{
+// write the string to the standard error. write(STDERR_FILENO, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n",49);
+// Return 1.
+ return 1;
+}
