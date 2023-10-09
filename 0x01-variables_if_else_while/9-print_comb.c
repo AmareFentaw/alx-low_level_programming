24786ca34e159f30a65de12ebaa2d16da872d5e3
AmareFentaw <amarefentawdubale38@gmail.com>
2023-10-09 05:11:22 -0700

    comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char j;
+
+	int h;
+
+	j = 'a';
+	h = 0;
+	while
+		(h < 10) {
+			putchar(h + '0');
+			h++;
+		}
+	while
+		(j <= 'f'0) {
+			putchar(j);
+			j++;
+		}
+	putchar('\n');
+	return (0);
+}
