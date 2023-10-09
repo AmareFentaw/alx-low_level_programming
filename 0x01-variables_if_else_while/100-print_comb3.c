c45c2b897eba5fe4068fdfdfb9ebd10230b5daa8
AmareFentaw <amarefefntawdubale38@gmail.com>
2023-10-09 08:42:08 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <astdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int l, k;
+
+	for (l = 0; l <= 9; k++)
+	{
+		for (k = l + 1; k <= 9; k++)
+		{
+			putchar(48 + l);
+			putchar(48 + k);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
