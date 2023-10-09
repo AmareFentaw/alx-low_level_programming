776b22764bdfab36b857eb621ccaac4f520b8bfa
AmareFentaw <amarefentawdubalw38@gmail.com>
2023-10-09 00:48:49 -0700

    alphabet

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,21 +7,21 @@
 
 int main(void)
 {
-	char c;
+	char m;
 
-	char d;
+	char b;
 
-	c = 'a';
-	d = 'A';
+	m = 'a';
+	b = 'A';
 	while
-		(c <= 'z') {
-			putchar(c);
-			c++;
+		(m <= 'z') {
+			putchar(m);
+			m++;
 		}
 	while
-		(d <= 'Z') {
-			putchar(c);
-			d++;
+		(b <= 'Z') {
+			putchar(b);
+			b++;
 		}
 	putchar('\n');
 	return (0);
