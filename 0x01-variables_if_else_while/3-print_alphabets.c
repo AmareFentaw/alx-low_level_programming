2796e93595fadc87f0c770f2192c93395dc973be
AmareFentaw <amarefentawdubalw38@gmail.com>
2023-10-09 00:29:23 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char m;
+
+	char b;
+
+	m = 'a';
+	b = 'A';
+	while
+		(m <= 'z') {
+			putchar(m);
+			m++;
+		}
+	while
+		(b <= 'z') {
+			putchar(b);
+			b++;
+		}
+	putchar(\'n');
+	return (0);
+}
