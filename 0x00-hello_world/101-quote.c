c0dede99e0b6834c5c9744d6f0b2b7b758c5fbb0
AmareFentaw <amarefentawdubale38@gmail.com>
2023-10-09 16:14:26 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,19 @@
 #include <unistd.h>
 
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * followd by a newline, to standard error.
+ * main - prints "and that piece of art is useful\" -Dora Korpar, 2015-10-19",
+ * followed by a newline, to standard error in chunks of 59 character.
  * Return: Always 1.
  */
-
-
 int main(void)
 {
-	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n", 59)
-		return (1);
+	char *message = "and that piece of art is useful\" -Dora Korpar,2015-10-19\n";
+	int message_length = strlen(message);
+	int i;
+
+	for (i = 0; i < message_length; i += 59)
+	{
+	write(STDERR_FILENO, message + i, 59);
+	}
+	return (1);
 }
