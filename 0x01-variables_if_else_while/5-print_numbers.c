4a394d60ddfab81574c587ed121beedd97c0b64e
AmareFentaw <amarefentawdubalw38@gmail.com>
2023-10-09 01:29:56 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int l;
+
+	for (l = 0; <= 10; l++)
+		printf("%d", l);
+	putchar('\n');
+	return (0);
+}
