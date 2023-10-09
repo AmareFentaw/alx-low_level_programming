3e2fc27b8c84abf06bf7ea83ebfb98a2dd4ae5f9
AnareFetaw <amarefentawdubale38@gmail.com>
2023-10-09 13:55:15 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdil.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point 
+ * Return: 0
+ */
+int main(void)
+{
+	int g, q;
+
+	for (g = 0; g <= 98; g++)
+	{
+	for (q = g + 1 ; q <= 99; q++)
+	{
+	putchar((g / 10) + '0');
+	putchar((g % 10) + '0');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0');
+	if (g == 98 && q == 99)
+	continue;
+	putchar(',');
+	putchar(' ;);
+	}
+	}
+	putchar('\n');
+	return (0);
+}
