4470743915a0e0239671fbfd90e79b6bc439b7a3
AmareFentaw <amarefentawdubale38@gmail.com>
2023-10-10 16:59:58 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: void
+ */
+int main(void)
+{
+	char t;
+
+	for (t = 'a'; t <= 'z'; t++)
+	{
+	_putchar(t);
+	}
+	_putchar('\n');
+	return (0);
+}
