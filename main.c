d3d1e5d1127975bf66c86e7617603269ac64aa1b
AmareFentaw <amarefentawdubale38@gmail.com>
2023-10-10 10:36:10 -0700

    Debugging

---
+++ main.c
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int odd_numbers(int m, int l, int s,)
+int prime_others(int n, int t, int h)
+void print_prime-numbers(int f, int b, int z,);
+
+#endif 
