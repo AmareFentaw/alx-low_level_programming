2f331560e86ff079ff81778f93b3100449fef5a2
AmareFentaw <amarefentawdubale38@gmail.com>
2023-10-10 14:21:29 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - to print _putchar
+ * REturn: 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('A');
+	_putchar('m');
+	_putchar('x');
+	_putchar('\n');
+	return (0);
+}
