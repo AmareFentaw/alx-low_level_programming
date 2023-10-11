b9cc5ff882f4006bf7f37628e3bac7c8ffff290a
AmareFentaw <amarefentawdubale38@gmail.com>
2023-10-11 13:07:18 -0700

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints even fibonacci
+ * less than 4000000.
+ * Return: Nothing
+ */*
+int main(void)
+{
+	int l = 0;
+
+	long m = 1, k = 2, sum = k;
+
+	while (k + m < 4000000)
+	{
+	k += m;
+	if (k % 2 == 0)
+	sum += k;
+	m = k - m;
+	++l;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
