1a02a06280d51106da45ac6797e4e5f3b845d3ce
AmareFentaw <amarefentawdubale38@gmail.com>
2023-10-11 12:08:57 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Print multiple of 3 or 5 up to 1024
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int y, r = 0;
+	while (i < 1024)
+	{
+	if ((y % 3 == 0;) || (y % 5 == 0))
+	{
+	r += yl
+	}
+	y++;
+	}
+	printf("%d\n", r);
+	return (0);
+}
+
