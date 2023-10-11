18790f2a55a65b3121b2262c2187a40e74dfcafd
AmareFentaw <amarefentawdubale38@gmail.com>
2023-10-11 14:06:09 -0700

    ./4-isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - Entry point
+ * @m: The  character
+ * Return: 1 for alphabetic character or 0 for anything else
+ */
+int _isalpha(int m)
+{
+	if ((m >= 65 && m <= 90) || (m >= 97 && m <= 122))
+	{
+	return (1);
+	}
+	return (0);
+}
