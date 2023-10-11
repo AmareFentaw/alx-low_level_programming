dbbdf0960209372b993c91828e7116670348a0dc
AmareFentaw <amarfentawdubale38@gmail.com>
2023-10-11 08:19:13 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - the purpose is to add.
+ * @l: number to be added.
+ * @s: number to be added.
+ * Return: Integer value
+ */
+int add(int l, int s)
+{
+	return (l + s);
+}
