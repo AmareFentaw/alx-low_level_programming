063d9016e395f0f87d09f6396c896f8ee29c69f6
AmareFentaw <amarefentawdubale38@gmail.com>
2023-10-11 13:43:38 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _islower - find the lowercase
+ * @n: The character to be manipulated
+ * Return: 1 for lowercase or 0 for anthing
+ */
+int _islower(int n)
+{
+	if (n >= 97 && n <= 122)
+	{
+	return (1);
+	}
+	return (0)'
+}
