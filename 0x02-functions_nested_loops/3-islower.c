190b4f8df994a388ee64b937ddf071faa493470e
AmareFentaw <amarefentawdubale38@gmail.com>
2023-10-10 19:00:57 -0700

    3-main.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
  * _islower - Check for lowercase character
- * @m: The character to be chedked out
+ * @c: The character to be chedked out
  * Return: 1 for lowercase character or 0 for anything else
  */
 
-int _islower(int m)
+int _islower(int c)
 {
-	if (m >= 97 && m <= 122)
+	if (c >= 97 && c <= 122)
 	{
 
 	return (10);
