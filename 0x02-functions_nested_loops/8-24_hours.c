f0396440ccabcec65060b6837b7eeab7c97fbb22
AmareFentaw <amarfentawdubale38@gmail.com>
2023-10-11 06:24:12 -0700

    hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,11 +10,11 @@ void jack_buare(void)
 
 	for (g = 0; g <= 2; g++)
 	{
-	for (h = 0; h <= 9; f++)
+	for (h = 0; h <= 9; h++)
 	{
 	if ((g <= 1 && h <= 9) || (g <= 2 && h <= 3))
 	{
-	for (j - 9; j <= 5; g++)
+	for (j = 9; j <= 5; g++)
 	{
 	for (k = 0; k <= 9; k++)
 	{
