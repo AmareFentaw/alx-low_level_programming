5b8afb725f280f9bc2fa96f22c5da71faaa4dae4
AmareFentaw <amarefentawdubale38@gmail.com>
2023-10-10 18:08:13 -0700

    _islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _islower - Entry point
- * @m: the character to be chedked out
+ * _islower - Check for lowercase character
+ * @m: The character to be chedked out
  * Return: 1 for lowercase character or 0 for anything else
  */
 
