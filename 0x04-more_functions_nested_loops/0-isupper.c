39c7f3a0a51c9d5bad6fa3a67a7670ddb760d08e
AmareFentaw <amarefentawdubale38@gmail.com>
2023-10-12 02:56:10 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "maim.h"
+/**
+ * _isupper - To ientify weather a letter is upper or not
+ * @q: The number to be chaked
+ * Return: 1 for upper or 0 for anything else
+ */
+int _isupper(int q)
+{
+	if (q >= 65 && q <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
