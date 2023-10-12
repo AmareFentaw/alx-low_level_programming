53b2a1898ee7f149b58d3558a861c019b6b381eb
AmareFentaw <amarefentawdubale38gmail.com>
2023-10-12 10:45:54 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_triangle - Prints swuare triangles
+ * @size: The size of the squares triangle
+ * Return: Nothing
+ */
+
+void print_triangle(int size)
+
+{
+
+	int a, b, c;
+
+	if (size <= 0)
+	{
+	putchar('\n');
+	}
+	else
+	{
+	for (a = 0; a < size; a++)
+	{
+	for (b = size - a; b > 1; b--)
+	{
+	putchar(32);
+	}
+	for (c = 0; c <= a; c++)
+	{
+	putchar(35);
+	}
+	putchar('\n');
+	}
+	}
+}
