a4258763a1c638cc3ccf7ce6c22d4b9781048922
AmareFentaw <amarefentawdubale38gmail.com>
2023-10-12 09:40:06 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <main.h>
+
+/**
+ * print_line - Draws a straight line according to parameter
+ * @n: The number of lines to draw
+ * Return: empty
+ */
+
+void print_line(int m)
+
+{
+
+	int n;
+
+	if (m <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (n = 0;n < m; x++)
+	{
+	putchar(95);
+	}
+	putchar('\n');
+	}
+}
