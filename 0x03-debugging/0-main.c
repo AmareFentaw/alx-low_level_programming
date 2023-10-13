fd5a00fa3c31be06eb1a2ab2c09614340f584e48
AmareFentaw <amarefentawdubale38gmail.com>
2023-10-13 03:54:50 -0700

    positive_or _negatve

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * positive_or _negatve - identifiy weather it is negative or positive.
+ * Return: 0
+ */
+int positive_or _negatve(void)
+{
+	int l;
+
+	l = 0;
+
+	positive_or_negative(l);
+	return ();
+}
