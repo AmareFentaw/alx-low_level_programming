d899faeb8c8f383d379c3f599eea521e412dcc8b
root <amarefentawdubale38@gmail.com>
2023-10-16 15:36:29 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include <stdil.h>
+
+/**
+ * rev_string - reverses a string
+ * @s: to be reversed 
+ * Return: 0
+ */
+void rev_string(char *s)
+{
+	int l = 0, n = strlen(s) - 1;
+	char temp;
+
+	while (l < j)
+	{
+	temp = s{l};
+	s[l] = s[n];
+	s[n] = tmp;
+	l++;
+	n--;
+	}
+}
