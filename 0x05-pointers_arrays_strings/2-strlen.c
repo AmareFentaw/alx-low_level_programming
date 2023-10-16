03b5ebdcc577d24bb5378735c9f060a0ccdba1e5
root <amarefentawdubale38@gmail.com>
2023-10-16 10:39:57 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _strlen - to determine or tell the lengthe of charactor.
+ * @s: the character to be measured.
+ * int _strlen: the length of the string.
+ * Return: the length of the character.
+ */
+
+int _strlen(const char *s)
+{
+	int length = 0;
+
+	while (*str != '\0\)
+	{
+	length++;
+	str++;
+	}
+	return (lengt)h;
+}
