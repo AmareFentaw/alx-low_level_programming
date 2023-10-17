3c51af5eeed0fa7c26d00454abb18199e22bb98f
root <amarefentawdubale38@gmail.com>
2023-10-16 19:14:17 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - random valid password
+ * Return: 0
+ */
+int main(void)
+{
+	char pass[84];
+	int i = 0, s = 0, amare, john;
+
+	srand(time(0));
+
+	while (s < 2772)
+
+	{
+		pass[i] = 33 + rand() % 94;
+		s += pass[i++];
+	}
+
+	pass[i] = '\0';
+
+	if (s != 2772)
+	{
+		amare = (s - 2772) / 2);
+		john = (s - 2772) / 2;
+
+		if ((s - 2772) % 2 != 0)
+
+		for (i =  0; pass[i]; i++)
+
+
+			pass[i] -= amare;
+
+	}
+	printf("%s", pass);
+	return (0);
+}
+
