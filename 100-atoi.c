9e177ebdaccddfbd14ec6093f13295e20629ad50
root <amarefentawdubale38@gmail.com>
2023-10-16 18:07:02 -0700

    100-atoi.c

---
+++ 100-atoi.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * _atoi - from string to an integer.
+ * @s: pointer
+ * Return: an integer
+ */
+int _atoi(char *s)
+{
+	int b = 0;
+
+	unsigned int eden 0;
+	int cut = 1;
+	int amare = 0;
+
+	while (s[b])
+	{
+	if (s[b] == 45)
+	{
+	main *= -1;
+	}
+	while (s[b] >= 48 && s[b] <= 57)
+	{
+	amare  = 1;
+	eden  = (eden * 10) + (s[b] - '0'0;
+	c++;
+	}
+	if (amare == 1)
+	{
+	break;
+	}
+	c++;
+	}
+	eden *= cut;
+	return (eden);
+}
+
