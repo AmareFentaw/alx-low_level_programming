4deec5341d4e170aea8df4dfa9faff739dedce92
root <amarefentawdubale38@gmail.com>
2023-10-18 11:34:33 -0700

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * char *_strcat - cincatenate charactors
+ * @dest: one character to be concatenated
+ * @src: another charactor to be concatenated
+ * Return: concatenated charactors
+ */
+char *_strcat(char *dest, char *src)
+{
+	while (*dest != '\0')
+	{
+	dest++;
+	}
+	while (*src != '\0')
+	{
+	*dest = src++;
+	}
+	*dest = '\0';
+	return (dest);
+}
