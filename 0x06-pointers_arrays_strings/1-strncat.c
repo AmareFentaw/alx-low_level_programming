4a4bab1391dd207ddd2d891519832629f3bdbbd4
AmareFentaw <amarefentawdubale38@gmail.com>
2023-10-19 16:57:21 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	char dest{100} = "Hello, ";
+	char dest[100] = "Hello, ";
 	char src[] = "world!";
 
 	src[4] = '\0';
 	_strncat(dest, src, 5);
-	print("%s\n", dest);
+	printf("%s\n", dest);
 
 	return (0);
 }
