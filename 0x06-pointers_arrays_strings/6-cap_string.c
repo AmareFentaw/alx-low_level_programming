ba22b31323321fedc14ccb3a1033cf48020c01a6
1-strncat.c <amarefentawdubale38@gmail.com>
2023-10-19 20:01:50 -0700

    char *cap_string(char *)

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - capitalize
+ * @l: to be changed
+ * Return: value
+ *
+ */
+char *cap_string(char *l)
+{
+	int x = 0, p;
+	int cspc = 13;
+
+	char spc[] = {32, '\t' '\n' 44, ';' 46, '!', '?', '"', '(', ')', '{', ''};
+
+	while (l[x])
+	{
+		p = 0;
+
+		while (p < cspc)
+		{
+			if ((x == 0 || l[x - 1] == spc[p] && (l[a] >= 97 && l[x] <= 122))
+				l[a] -= 32;
+
+			p++;
+		}
+		x++;
+	}
+	return (l);
+}
