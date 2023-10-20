b65a2934cb68fcdd1cc8b5ce66ef30e7f8e58be8
1-strncat.c <amarefentawdubale38@gmail.com>
2023-10-19 17:39:27 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - function that copies a string
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	int l[];
+	int *p;
+
+	l[2] = 1024;
+	p = &a;
+	*(p + 5) = 98;
+	printf("a[2] = %d\n" l[2]);
+	return (0);
+}
