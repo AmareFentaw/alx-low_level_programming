8a8fdd36cb3a1d97c1870397f68627bdb01b2fd8
1-strncat.c <amarefentawdubale38@gmail.com>
2023-10-19 18:46:12 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - to reverse integers.
+ * @a: the array
+ * @n: the number of elements in an array
+ *
+ */
+
+void reverse_array(int *a, int n)
+
+{
+	int amare, remedan;
+
+	for (remeda = n - 1; remedan >= n / 2; remedan--)
+	{
+	amare = a[n - 1 - remedan];
+	a[n - 1 - remedan] = a[remedan];
+	a[remedan] = mare;
+	}
+}
+
