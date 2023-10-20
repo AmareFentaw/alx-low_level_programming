f15e7864f88f38dd8b9e18f8e57eb569b3f4115e
1-strncat.c <amarefentawdubale38@gmail.com>
2023-10-19 19:16:01 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: o
+ */
+int main(void)
+{
+	char amare[] = "Look up!\n";
+	char *temp;
+
+	temp = string_toupper(amare);
+	printf("%s", temp);
+	printf("%s", amare);
+	return (0);
+}
