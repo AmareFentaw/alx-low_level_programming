aa9752e6cc657f52150b1bb3109b6d750c15ac07
1-strncat.c <amarefentawdubale38@gmail.com>
2023-10-19 20:18:15 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *l)
 	int x = 0, p;
 	int cspc = 13;
 
-	char spc[] = {32, '\t' '\n' 44, ';' 46, '!', '?', '"', '(', ')', '{', ''};
+	char spc[] = {32, '\t' '\n' 44, ';' 46, '!', '?', '"', '(', ')', '{', '}'};
 
 	while (l[x])
 	{
@@ -20,7 +20,7 @@ char *cap_string(char *l)
 
 		while (p < cspc)
 		{
-			if ((x == 0 || l[x - 1] == spc[p] && (l[a] <= 97 && l[x] <= 122))
+			if ((x == 0 || l[x - 1] == spc[p] && (l[a] >= 97 && l[x] <= 122))
 				l[a] -= 32;
 
 			p++;
