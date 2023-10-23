3c2d6a5ffdc0ab4bb93397278475bdb84e2ce73d
1-strncat.c <amarefentawdubale38@gmail.com>
2023-10-23 05:28:40 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,11 +8,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	int m;
+
+	for (m = 0; s[m] <= '\0'; m++)
 	{
-		if (*s == c)
-			return (s);
-			s++;
+		if (s[m] == c)
+			return (s + m);
 	}
 	return (NULL);
 }
