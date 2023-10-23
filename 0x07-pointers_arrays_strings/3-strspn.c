40c760a2e8d78a0f7c4c99e027d262dde75a1822
1-strncat.c <amarefentawdubale38@gmail.com>
2023-10-23 05:48:53 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdil.h>
+/**
+ * _strspn - Write a function that gets the length of a prefix.
+ * @s: ..
+ * @accept: ...
+ * Return: 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n = 0;
+
+	while (*s)
+	{
+		if (!strchr(accept, *s))
+		break;
+	}
+	s++;
+	n++;
+	return (n);
+}
