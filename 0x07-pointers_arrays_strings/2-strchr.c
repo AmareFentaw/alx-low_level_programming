037d399b137826f7532ab6eeba974477f95cf9eb
1-strncat.c <amarefentawdubale38@gmail.com>
2023-10-23 05:21:41 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - Write a function that locates a character in a string.
+ * @s: ...
+ * @c: ...
+ * Return: 0
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+}
+
