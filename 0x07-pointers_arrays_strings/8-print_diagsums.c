f3d3886b25b21a518b210293f763969c03c30f4d
1-strncat.c <amarefentawdubale38@gmail.com>
2023-10-23 08:29:55 -0700

    8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,20 +7,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int l;
-	int amare1 = 0;
-	int amare2 = 0;
+	int l, m;
 
-	for (l = 0; l < size; l++)
-	{
-		amare1 += a[l * (size + 1)];
-		amare2 += a[(size - l - 1) * (size + 1)];
-	}
-	int total = amare1 + amare2;
 
-	for (int l = total; l > 0; l /= 10)
+	for (l = 0; l < 8; l++)
 	{
-	putchar(l % 10 + '0');
+		for (m = 0; m < 8; m++)
+			_putchar(a[l][m]);
+		putchar('\n');
 	}
-	putchar('\n');
 }
