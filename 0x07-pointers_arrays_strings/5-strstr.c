3e07ff11f7267f597e152ec573f91bb81a830b84
1-strncat.c <amarefentawdubale38@gmail.com>
2023-10-23 06:35:02 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * _strstr - Write a function that locates a substring
+ * @haystack: input
+ * @needle: another input
+ * Return: 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	while (*haystack)
+	{
+		if (*haystack == *needle)
+		{
+			char *h = haystack;
+			char *n = needle;
+
+			while (*h && *n && *h == *n)
+			{
+			h++;
+			n++;
+			}
+			if (*n == '\0')
+			{
+			return (haystack);
+			}
+		}
+	haystack++;
+	}
+	return (NULL);
+}
